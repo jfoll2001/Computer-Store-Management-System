62c9020adf5a06d26a9df35a9839ec3c4eab3e8e
Samshalie <samshalie@icloud.com>
2022-06-29 14:22:20 -0400

    commit

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
+global.conn = require('./dbconnection/db.js');
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
 
 var app = express();
+app.use(cors())
 
 
 // view engine setup
@@ -19,12 +28,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
